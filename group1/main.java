c090e684e24e70fbeb484675f59e416ca5acbd11
joehub2026 <joelrutwaza56@gmail.com>
2026-03-17 12:00:33 +0200

    Update main.java

--- group1/main.java
+++ group1/main.java
@@ -5,12 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name="joe";
-		String a="hello class,";
-		String b="my name is joe";
-		System.out.println(a+b);
-		//System.out.println("hello class, my name is "+name);
-
+		System.out.println("hello class, my name is "+name +"and i thank you!!");
 	}
-
 }
 
