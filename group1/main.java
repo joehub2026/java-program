343e3d4768d269f7d6e5496c339e0c5020af64ba
Your Name <joelrutwaza56@gmail.com>
2026-03-17 11:54:18 +0200

    my first java program

---
+++ group1/main.java
@@ -0,0 +1,16 @@
+package group1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="joe";
+		String a="hello class,";
+		String b="my name is joe";
+		System.out.println(a+b);
+		//System.out.println("hello class, my name is "+name);
+
+	}
+
+}
+
